54a77ff077d077412eff5f38e843bf23b188e6ea
Praveen <pkchmits@gmail.com>
2021-02-10 14:43:05 +0530

    Optimized the repeted code and used @Before Anotation

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -7,8 +7,10 @@ import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.addedittask.AddEditTaskViewModel
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.CoreMatchers.*
+import org.junit.Before
 import org.junit.Rule
 
 @RunWith(AndroidJUnit4::class)
@@ -17,10 +19,17 @@ class TasksViewModelTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var tasksViewModel: TasksViewModel
+
+    @Before
+    fun setupViewModel(){
+        //GIVEN a fresh ViewModel
+     tasksViewModel= TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
-        // Given a fresh TasksViewModel
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
 
         // When adding a new task
         tasksViewModel.addNewTask()
@@ -34,9 +43,6 @@ class TasksViewModelTest {
 
     @Test
     fun setFilterAllTasks_tasksAddViewVisible(){
-        //GIVEN
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-
 
         // WHEN
         tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
