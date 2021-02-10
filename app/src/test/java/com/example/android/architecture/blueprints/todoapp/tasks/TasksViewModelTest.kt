24fe2c04e2fa456366243e7326b1f82a22a5750c
Praveen <pkchmits@gmail.com>
2021-02-10 14:22:32 +0530

    Added dependencies and TestingUtil class for LiveData Testing

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,14 +1,22 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import com.example.android.architecture.blueprints.todoapp.taskdetail.TaskDetailViewModel
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.junit.Rule
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
@@ -19,6 +27,9 @@ class TasksViewModelTest {
         tasksViewModel.addNewTask()
 
         // Then the new task event is triggered
+        val value=tasksViewModel.newTaskEvent.getOrAwaitValue()
+
+        assertThat(value.getContentIfNotHandled(), not(nullValue()))
     }
 
 
