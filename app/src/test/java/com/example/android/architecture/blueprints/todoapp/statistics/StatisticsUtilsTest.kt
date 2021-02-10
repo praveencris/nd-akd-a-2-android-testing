5f302b95065e446946810045e02d2fc2de31cae3
Praveen <pkchmits@gmail.com>
2021-02-10 10:35:23 +0530

    Written two test cases for Active or complete task percentages

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,36 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class StatisticsUtilsTest {
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returns_HundredZero() {
+        val task = listOf<Task>(
+                Task("Title", "Description", isCompleted = false),
+                Task("Title", "Description", isCompleted = false),
+                Task("Title", "Description", isCompleted = false),
+        )
+        val stats = getActiveAndCompletedStats(task)
+        assertEquals(100f, stats.activeTasksPercent)
+        assertEquals(0f, stats.completedTasksPercent)
+    }
+
+
+    @Test
+    fun getActiveAndCompletedStats_someCompleted_returns_SixtyForty() {
+        val task = listOf<Task>(
+                Task("Title", "Description", isCompleted = true),
+                Task("Title", "Description", isCompleted = true),
+                Task("Title", "Description", isCompleted = false),
+                Task("Title", "Description", isCompleted = false),
+                Task("Title", "Description", isCompleted = false)
+        )
+        val stats = getActiveAndCompletedStats(task)
+        assertEquals(60f, stats.activeTasksPercent)
+        assertEquals(40f, stats.completedTasksPercent)
+    }
+}
