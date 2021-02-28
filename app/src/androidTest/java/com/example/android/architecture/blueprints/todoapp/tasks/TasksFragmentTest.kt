26b4600263583eb3a6fa5c01754e763f67e8d6be
Praveen <pkchmits@gmail.com>
2021-02-28 08:35:12 +0530

    Done testing coroutine

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragmentTest.kt
@@ -1,10 +1,12 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -45,10 +47,8 @@ class TasksFragmentTest {
 
     @Test
     fun clickTask_navigateToDetailFragmentOne() = runBlockingTest {
-
         repository.saveTask(Task("Title1", "Description1", false, "id1"))
         repository.saveTask(Task("Title2", "Description2", true, "id2"))
-
 
         // GIVEN - On the home screen
         val scenario = launchFragmentInContainer<TasksFragment>(Bundle(), R.style.AppTheme)
@@ -77,4 +77,32 @@ class TasksFragmentTest {
 
     }
 
-}
+    @Test
+    fun clickAddTaskButton_navigateToAddEditFragment() = runBlockingTest {
+
+            // GIVEN - On the home screen
+            val scenario = launchFragmentInContainer<TasksFragment>(Bundle(), R.style.AppTheme)
+
+            //Use Mockito's mock function to create a mock
+            val navController = mock(NavController::class.java)
+
+            //Make your new mock the fragment's NavController
+            scenario.onFragment {
+                Navigation.setViewNavController(it.view!!, navController)
+            }
+
+            // WHEN - Click on the first list item
+            //Add the code to click on FAB Button
+            onView(withId(R.id.add_task_fab))
+                    .perform(click())
+
+            //Verify that navigate was called, with the correct argument
+
+            //THEN - Verify that we navigate to the AddEditTaskFragment screen
+            verify(navController).navigate(
+                    TasksFragmentDirections.actionTasksFragmentToAddEditTaskFragment(
+                            null, getApplicationContext<Context>().getString(R.string.add_task)
+            ))
+
+        }
+    }
--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,16 +1,39 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.architecture.blueprints.todoapp.Event
+import com.example.android.architecture.blueprints.todoapp.R
 import org.junit.Assert.*
 import org.junit.Test
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
 import org.hamcrest.CoreMatchers.*
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 
+@ExperimentalCoroutinesApi
 class TasksViewModelTest {
+
+    val testDispatcher = TestCoroutineDispatcher()
+
+    @Before
+    fun setupDispatcher() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDownDispatcher() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
 
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
@@ -27,10 +50,10 @@ class TasksViewModelTest {
     fun setupViewModel() {
         testRepository = FakeTestRepository()
         val task1 = Task("Title1", "Description1")
-        val task2 = Task("Title2", "Description2", true)
-        val task3 = Task("Title3", "Description3", true)
+        val task2 = Task("Title2", "Description2", false)
+        val task3 = Task("Title3", "Description3", false)
 
-        testRepository.addTasks(task1,task2,task3)
+        testRepository.addTasks(task1, task2, task3)
 
         //GIVEN a fresh ViewModel
         tasksViewModel = TasksViewModel(testRepository)
@@ -59,6 +82,27 @@ class TasksViewModelTest {
         val value = tasksViewModel.tasksAddViewVisible.getOrAwaitValue()
 
         assertThat(value, `is`(true))
+
+    }
+
+
+    @Test
+    fun completeTask_dataAndSnackbarUpdated()= runBlockingTest{
+        // Create an active task and add it to the repository.
+        val task = Task("Title", "Description")
+
+        testRepository.addTasks(task)
+
+        // Mark the task as complete task.
+        tasksViewModel.completeTask(task, true)
+
+        // Verify the task is completed.
+        assertThat(testRepository.tasksServiceData[task.id]?.isCompleted,
+                `is`(true))
+
+        // Assert that the snackbar has been updated with the correct text.
+        val snackbarText: Event<Int> = tasksViewModel.snackbarText.getOrAwaitValue()
+        assertThat(snackbarText.getContentIfNotHandled(), `is`(R.string.task_marked_complete))
 
     }
 
